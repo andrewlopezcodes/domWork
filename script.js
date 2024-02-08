936fdf8a7db864b0327712c82567da591e64f076
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-08 16:49:22 -0500

    refactor button eventlistener conditional

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ var unorderedList = document.querySelector("ul");
 
 
 button.addEventListener("click", function(){
-  if(textInput.value === ""){
-    alert("Shopping List Field Empty");
-  } else {
-    let newLi = document.createElement("li");
+  if(textInput.value > 0){
+  let newLi = document.createElement("li");
   newLi.appendChild(document.createTextNode(textInput.value));
   unorderedList.appendChild(newLi);
   textInput.value = "";
