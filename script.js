06b54840c74a328cb3981c064b117555d8da9a4c
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-08 21:55:05 -0500

    reduce eventlistener functions

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 var button = document.getElementById("enterButton");
 var textInput = document.getElementById("userInput");
 var unorderedList = document.querySelector("ul");
+let addToList = function(){
+    let newLi = document.createElement("li");
+    newLi.appendChild(document.createTextNode(textInput.value));
+    unorderedList.appendChild(newLi);
+    textInput.value = "";
+  };
+
+textInput.addEventListener("keydown", function(onkeydown){
+  if(onkeydown.key === "Enter" && textInput.value !== ""){
+    addToList();
+  }
+});
 
 
 
 button.addEventListener("click", function(){
-  if(textInput.value > 0){
-  let newLi = document.createElement("li");
-  newLi.appendChild(document.createTextNode(textInput.value));
-  unorderedList.appendChild(newLi);
-  textInput.value = "";
-  } 
+  if(textInput.value !== ""){
+    console.log(textInput.value);
+    addToList();
+  }  
 });
 
 
